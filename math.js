23f2ef72658359546e111a520f02f307db8d5437
InloranReliza <inloranreliza@gmail.com>
2025-11-18 08:09:23 +0800

    added new function for math.js

---
+++ math.js
@@ -0,0 +1,10 @@
+let num1 = 5 ;
+let num2 = 10;
+let sum = num1 + num2;
+
+function add(num1,num2) {
+    return num1 + num2;
+}
+ 
+console.log("the sum is: " + add(num1, num2));
+module.exports = { add };
