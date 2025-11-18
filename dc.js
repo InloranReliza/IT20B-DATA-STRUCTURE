ddd9adfcb623d3dfa5c96fedf4876616bc18603c
InloranReliza <inloranreliza@gmail.com>
2025-11-18 08:23:56 +0800

    added arithmetic

---
+++ dc.js
@@ -0,0 +1,34 @@
+function legalAge(age) {
+
+if (age >=18) {
+    console.log("You are of legal age.");
+} else {
+    console.log("you are not an adult.");
+}
+}
+
+legalAge(69);
+
+function computeGRade(grade){
+ 
+    if(grade >=94.5){
+        console.log("1");
+
+    } else if (grade >=91.5){
+        console.log("1.25");
+
+    } else if(garde => 89.5){
+        console.log("1.5");
+
+    } else if (grade >=86.5){
+        console.log("1.75");
+
+    } else if (grade >=84.5){
+        console.log("2");
+
+    } else {
+        console.log("Not within the transmutation");
+    }
+    }
+
+computeGRade(87);
--- math.js
+++ math.js
@@ -1,10 +1,38 @@
-let num1 = 5 ;
-let num2 = 10;
-let sum = num1 + num2;
 
-function add(num1,num2) {
-    return num1 + num2;
+//export function power() {
+    return Math.pow(num1, num2) ;
 }
- 
-console.log("the sum is: " + add(num1, num2));
-module.exports = { add };
+//export function modulus() {
+    return num2 % num1 ;
+}
+
+//export function incrementNum1() {
+    num1 += 1 ;
+}
+//export function decrementNum2() {
+
+//
+num1=2;
+num2=3;
+
+function sum(num1,num2){
+    sum = num1 + num2 ;
+    console.log("sum:",sum);
+}
+function multiply(num1,num2){
+    multiply = num1 * num2 ;
+    console.log("multiply:",multiply);
+}
+function subtract(num1,num2){
+    subtract = num2 - num1 ;
+    console.log("subtract:",subtract);
+}
+function divide(num1,num2){
+    divide = num2 / num1 ;
+    console.log("divide:",divide);
+}
+sum(num1,num2);
+multiply(num1,num2);
+diff(num1,num2);
+divide(num1,num2);
+
