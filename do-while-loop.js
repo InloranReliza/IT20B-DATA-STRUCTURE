7f72678292ce10e5e70f264a7e09b3b97c9d094a
InloranReliza <inloranreliza@gmail.com>
2025-11-21 08:47:03 +0800

    added Do-While-loop.js

--- do-while-loop.js
+++ do-while-loop.js
@@ -3,16 +3,34 @@
 i = 1;
 
 do{
-    console.log(i);
-    i++;
+output += i + " ";
+i++;
+
 } while (i<=5);
 */
 
 
 //desc
-i = ;
+i =5;
+
+output = "";
 
 do{
+    output += i + " ";
+    i++;
     console.log(i);
     i--;
 } while (i>=1);
+
+//
+arr = [4,2,3];
+output = "";
+i=0;
+do{
+    output += arr[i] + " ";
+    i++;
+}while (i<=arr.length-1);
+
+console.log(output);
+
+arrDoWhile = {arr1};
