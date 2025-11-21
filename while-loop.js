517cd312ebec30ec70f67e06c5ffc4b7000a498f
InloranReliza <inloranreliza@gmail.com>
2025-11-21 08:21:48 +0800

    added while-loop output

--- while-loop.js
+++ while-loop.js
@@ -6,7 +6,7 @@ while   (i>=1){
     console.log(i);
     i--;
 }
-*/
+
 
 i=1;
 
@@ -14,3 +14,15 @@ while (i<=5){
     console.log(i);
     i++;
 }
+ */
+
+output = "";
+i=1;
+
+while (i<=5){
+    output += output;
+    console.log(output);
+    i++;
+}
+
+console.log(output);
