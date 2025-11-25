d8c5ba77481563e548df24a6713ae0c302cc43f9
InloranReliza <inloranreliza@gmail.com>
2025-11-25 08:50:57 +0800

    added new stack

--- DSA/stack.js
+++ DSA/stack.js
@@ -20,10 +20,41 @@ class Stack{
         console.log(output);
     }
 
+    is_empty(){
+        return this.topindex === -1;
+    }
+ size(){
+        return this.topindex + 1;
+    }
+
+    peek(){
+        //correcting statement
+        if(this.is_empty()) return null;
+        return this.data[this.topindex]; 
+    }   
+    pop(){
+        //correcting statement
+        if(this.is_empty()) return null;
+        
+        let value = this.data[this.topindex];
+        delete this.data[this.topindex];
+        this.topindex--;
+        return value;
+    }
 //
 Stack1 = new Stack();
 
+console.log(Stack1.is_empty());
 Stack1.push(4);
 Stack1.push(2);
 Stack1.push(3); 
-Stack1.traverse();
+Stack1.push(5);
+Stack1.push(8);
+console.log("the current size is"+stack1.size());
+console.log("the top element is "+stack1.peek());
+console.log("The top element" + stack1.pop() + " is popped, the new top elememnt is " + stack1.peek());
+console.log("The new stack size is " + Stack1.size());
+
+
+
+
