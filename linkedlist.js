f8eb3ed485f9e41c6d8f1d6d39f85cc301b95bd9
InloranReliza <inloranreliza@gmail.com>
2025-12-02 08:28:17 +0800

    added classlinkedlist.js

---
+++ linkedlist.js
@@ -0,0 +1,34 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+
+    constructor(){
+        this.head = null;
+    }
+
+    insert_at_beginning(){
+        const node = new Node(element);
+        node.next = this.head;
+        this.head = node;
+
+    }
+
+    traverse(){
+        let current = this.head;
+        let output ="";
+
+        while (current){
+            output += current.value + "";
+            current = current.next;
+        }  
+
+        return output;
+      }
+
+}
+ 
